2f551f5ee528df523be78adc8704afe73078baea
Aleksander <tatrun89@gmail.com>
2019-10-29 13:52:18 +0200

    add pom file

--- src/main/java/me/wisehands/storage/FootGoApplication.java
+++ src/main/java/me/wisehands/storage/FootGoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "me.wisehands.storage"})
+@ComponentScan(basePackages = {"me.wisehands.storage"})
 
 public class FootGoApplication extends SpringBootServletInitializer {
 
